61abd3442fa2addcfb603e396b5354f1411641bd
Tony Bowden <tony@tmtm.com>
2022-06-17 13:10:50 +0300

    update to DecoratedComparison

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -5,7 +5,7 @@ require 'every_politician_scraper/comparison'
 
 # Skip junior ministers for now
 # TODO: make sure all are included
-class Comparison < EveryPoliticianScraper::NulllessComparison
+class Comparison < EveryPoliticianScraper::DecoratedComparison
   def external
     @external ||= super.delete_if { |row| row[:position].start_with? 'Deputy Minister' }
   end
